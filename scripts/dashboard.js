61172f047505b8f83a3cb21c214293de194f7b62
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-02 13:54:12 +0200

    displayed user's set budget

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,4 +1,6 @@
 const userNameDisplay = document.getElementById("username-display");
+const budgetDisplay = document.getElementById("budget-display");
 let userDataObject = JSON.parse(localStorage.getItem("userData"));
 
 userNameDisplay.innerText = userDataObject.username;
+budgetDisplay.innerText = userDataObject.budget;
