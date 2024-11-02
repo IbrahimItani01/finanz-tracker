85ba0446dcdbc7190f6cc9419c87958e5e697335
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-02 17:45:58 +0200

    implemented view logic of income

--- scripts/income.js
+++ scripts/income.js
@@ -15,8 +15,31 @@ const addIncome = (amount, note) => {
   saveToLocalStorage();
   console.log(userDataObject);
 };
+
 incomeForm.addEventListener("submit", () => {
     const amount = incomeAmount.value
     const note = incomeNote.value
     addIncome(amount,note)
 });
+
+const renderIncomeData = ()=>{
+    userDataObject.income.forEach((incomeData)=>{
+        incomeDiv.innerHTML+=`
+              <div class="income-card" key=${incomeData.id}>
+                <div class="information">
+                  <h2>$${incomeData.amount}</h2>
+                  <p>${incomeData.note}</p>
+                </div>
+                <div class="actions-container">
+                  <div class="actions" key="edit-${incomeData.id}">
+                    <img src="/assets/edit-icon.svg" alt="edit-icon">
+                  </div>
+                    <div class="actions" key="delete-${incomeData.id}">
+                      <img src="/assets/delete-icon.svg" alt="delete-icon">
+                    </div>
+                </div>
+              </div>
+        `
+    })
+}
+renderIncomeData()
