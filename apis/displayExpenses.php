a0ce319233e3b007ce3bad236bdc3c8b7336997e
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-07 23:22:58 +0200

    created displayExpenses api

---
+++ apis/displayExpenses.php
@@ -0,0 +1,22 @@
+<?php
+include "connection.php";
+
+$userId =$_POST["userId"];
+
+$query = $connection->prepare("SELECT id,amount,note FROM expenses WHERE user_id=?");
+$query->bind_param("i", $userId);
+$query->execute();
+$result = $query->get_result();
+if($result->num_rows > 0){
+    $array =[];
+    while($row = $result->fetch_assoc()){
+        $array[] = $row;
+    }
+    echo json_encode($array);
+}else{
+    $response=[
+        "status"=> "error",
+        "message"=> "empty result",
+    ];
+    echo json_encode($response);
+}
