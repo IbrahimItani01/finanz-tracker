57b9b477d8175ded0dff857fc7085c6bdd3480d0
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-07 22:16:42 +0200

    implemented deleteUser api

---
+++ apis/deleteUser.php
@@ -0,0 +1,16 @@
+<?php
+include "connection.php";
+$id = $_POST["id"];
+
+$query = $connection->prepare("DELETE FROM users WHERE id=?");
+$query->bind_param("s", $id);
+if($query->execute()){
+    $response = [
+        "status"=> "success",
+        "message"=> "successfully deleted user with id $id"
+    ];
+    echo json_encode($response);
+}
+else{
+    echo "Error deleting user with id $id";
+}
