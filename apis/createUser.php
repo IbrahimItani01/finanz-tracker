df593504c119d51758273f72317d6802ec181f87
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-08 22:08:27 +0200

    implemented fetching user password based on input userName

--- apis/createUser.php
+++ apis/createUser.php
@@ -2,23 +2,38 @@
 
 
  include "connection.php";
- $data = json_decode(file_get_contents("php://input"), true);
+//  $data = json_decode(file_get_contents("php://input"), true);
 
- $userName = $data["name"];
- $budget = $data["budget"];
+//  $userName = $data["name"];
+//  $password = $data["password"];
+//  $budget = $data["budget"];
+$userName = $_POST["name"];
+$password = $_POST["password"];
+$budget = $_POST["budget"];
 
-$query = $connection->prepare("INSERT INTO users (name, budget) VALUES (?, ?)");
-$query->bind_param("si", $userName, $budget);
-
-if($query->execute() === TRUE) {    
-    $userId = $connection->insert_id;
-    $response = [
-        "status"=> "success",
-        "message"=> "user: added $userName to DB",
-        "userId"=> $userId,
-    ];
-    echo json_encode( $response );
-}else{
-    echo "Failed adding user";
+$hashed = password_hash($password,PASSWORD_DEFAULT);
+$checkUserExist = $connection->prepare("SELECT password from users where name=?");
+$checkUserExist->bind_param( "s", $userName);
+$checkUserExist->execute();
+$result = $checkUserExist->get_result();
+if ($result->num_rows> 0) {
+    $row = $result->fetch_assoc();
+    echo json_encode($row["password"]);
 }
-exit; // Ensures no other output is sent
+
+// $query = $connection->prepare("INSERT INTO users (name, budget, password) VALUES (?, ?,?)");
+// $query->bind_param("sis", $userName, $budget,$hashed);
+
+// if($query->execute() === TRUE) {    
+//     $userId = $connection->insert_id;
+//     $response = [
+//         "status"=> "success",
+//         "message"=> "user: added $userName to DB",
+//         "userId"=> $userId,
+//         "password is"=> $hashed,
+//     ];
+//     echo json_encode( $response );
+// }else{
+//     echo "Failed adding user";
+// }
+// exit; // Ensures no other output is sent
